ef976fc08054b767ffd9c172486581a7d4cf801e
sushmitaS16 <sushmita.iitr16@gmail.com>
2018-07-19 17:25:16 +0530

    Updated

--- findIntersects.R
+++ findIntersects.R
@@ -47,7 +47,7 @@ rangedIntersects = function(file1, file2) {
 			if (site1[[i]] == site2[[j]] | ((site1[[i]] > down) & (site1[[i]] < up))) {
 				res = rbind(res, data.frame(chromosome1 = file1$chromosome[[i]], chromosome2 = file2$chromosome[[j]], editCoord1 = site1[[i]], editCoord2 = site2[[j]], geneName1 = file1$gene[[i]], geneName2 = file2$gene[[j]], editPercent1 = file1$editPercent[[i]], editPercent2 = file2$editPercent[[j]]))	
 			} else {
-						next
+				next
 			}
 		}
 	}
@@ -56,23 +56,55 @@ rangedIntersects = function(file1, file2) {
 
 }
 
+geneIntersects = function(file1, file2) {
+
+	file1 = subset(f1, duplicated(file1$gene) == FALSE)
+	file2 = subset(f2, duplicated(file2$gene) == FALSE)
+
+	g1 = as.character(file1$gene)
+	g2 = as.character(file2$gene)
+
+	res = data.frame(geneName = character())
+
+	for (i in 1:length(g1)) {
+	     if((g1[[i]] %in% g2) == TRUE) {
+	         res = rbind(res, data.frame(geneName = g1[[i]]))
+	     } else {
+	         next
+	       }
+	}
+
+	return(res)
+}
+
+
+
 #cd /path/to/directory
 f1 = read.csv(readline(prompt = "Enter the 1st file : "), header=TRUE)
 f2 = read.csv(readline(prompt = "Enter the 2nd file : "), header=TRUE)
 
-#ask user what output he needs
-output = readline(prompt = "Want to get exact intersects? (Y/N) : ")
+#ask user how he wants to find his intersects
+type = readline(prompt = "Intersects based on edit site or gene name? (site-based/gene-based) : ")
 
-if (output == "Y" | output == "yes" | output == "Yes" | output == "y") {
-	result = exactIntersects(f1, f2)
+if(type == "site-based") {
 
-} else if (output == "N" | output == "no" | output == "No" | output == "n") {
-	result = rangedIntersects(f1, f2)
+	#ask user what output he needs
+	output = readline(prompt = "Want to get exact intersects? (Y/N) : ")
+	if (output == "Y" | output == "yes" | output == "Yes" | output == "y") {
+		result = exactIntersects(f1, f2)
+	} else if (output == "N" | output == "no" | output == "No" | output == "n") {
+		result = rangedIntersects(f1, f2)
+	} else {
+		print("Error input!")
+	}
+
+} else if(type == "gene-based") {
+	#this gives out the list of genes common in both files
+	result = geneIntersects(f1,f2)
 
 } else {
-	print("Error input!")
+	print("Error!")
 }
-
 
 #outputs a csv file containing the common editing sites of two RBPs (pairwise comparison)
 name = readline(prompt = "Name your csv file : ")
